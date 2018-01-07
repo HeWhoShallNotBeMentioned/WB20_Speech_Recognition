a4959c3f13948e1cba01c6c2831a3063bdf41efb
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-06 17:08:40 -0800

    created server to run app. Create listening to catch speech and print to screen

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
   let p = document.createElement('p');
   const words = document.querySelector('.words');
   words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  console.log(e.results);
+  const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+      const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+      p.textContent = poopScript;
+
+      if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+      }
+});
+recognition.start();
